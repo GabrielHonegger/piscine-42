27c6c7386e5b803ffb7f1645d354532e24a47679
Gabriel Lopes Honegger <glopes-h@e3r9p3.42roma.it>
2023-10-07 14:50:40 +0200

    saving everything

---
+++ Rush/Rush01/ex00/rush-01.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void	ft_putchar(char str)
+{
+	write(1, &str, 1);
+}
+
+void	ft_sky_scraper(int argc, char **argv)
+{
+	char matrix[1][] = argv[1][];
+
+}
+
+int	main(int argc, char **argv)
+{
+	ft_sky_scraper(argc, argv);
+}
